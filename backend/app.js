e3f2a88d95fd930324486d03380e7de0dfb79ba6
Kumar Piyush <kumarpiyushxd@gmail.com>
2024-11-01 11:44:26 -0700

    added profilePage backend and frontend

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,18 @@ import compression from "compression";
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import path from 'path';
 
 import auth from "./routes/auth.js";
+import { fileURLToPath } from "url";
 
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve the 'uploads' directory as static
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use(morgan("dev"));
 
